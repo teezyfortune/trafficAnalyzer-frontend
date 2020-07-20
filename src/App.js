8da58e6834000632b64590a3d60099ecaac9e78e
koladeore <koladeore@gmail.com>
2020-07-20 21:35:34 +0100

    design-warden-page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import "./App.css";
 import SignupPage from "./components/adminPages/adminSignUp/adminSignup";
 import LoginPage from "./components/adminPages/adminLoginPage/adminLogin";
 import AdminDashBoard from "./components/adminPages/adminDashBoard/AdminDashBoard";
-import wardenDashBoard from "./components/DashBoard/WardenDashBoard";
+import wardenDashBoard from "./components/wardenPages/wardenDashBoard/WardenDashBoard.jsx";
 import AddWarden from "./components/DashBoard/addWarden";
+import WardenLoginPage from "./components/wardenPages/wardenLoginPage/wardenLogin";
 
 const App = () => (
   <Router>
@@ -15,6 +16,7 @@ const App = () => (
       <Route path="/adminDashBoard" exact component={AdminDashBoard} />
       <Route path="/addWarden" exact component={AddWarden} />
       <Route path="/wardenDashboard" exact component={wardenDashBoard} />
+      <Route path="/wardenLogin" exact component={WardenLoginPage} />
     </Switch>
   </Router>
 );
